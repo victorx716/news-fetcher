985d7f10b7a6e15de90cbf7905513973e0fa19c4
Victor Liu <vliu@ibm.com>
2019-10-18 16:20:03 -0500

    Update config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 require('dotenv').config()
 
+const withOffline = require('next-offline')
+
+const nextConfig = {
+  ...
+}
+
 module.exports = {
   env: {
     API_KEY: process.env.API_KEY
-  }
+  },
+  withOffline(nextConfig)
 };
